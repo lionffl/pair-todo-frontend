2b7265d7c0a97ec52bef90b04ef04a558ec987b3
lion <lionffl@gmail.com>
2023-01-12 11:29:28 -0300

    revert last commit

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const fetchTasks = async (method, url, _payload) => {
   try {
     const tasks = await axios({
-      headers: { 'Access-Control-Allow-Origin': 'https://63c0111ef4708e118932bf1d--sage-dragon-5c5b26.netlify.app/' },
       method,
       url,
       data: _payload,
