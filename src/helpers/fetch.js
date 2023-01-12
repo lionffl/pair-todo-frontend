f8c951cd36830fe2c33f3b7d63b54182dc9aee30
lion <lionffl@gmail.com>
2023-01-12 12:50:30 -0300

    fix: url typo

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const fetchTasks = async (method, url, _payload) => {
   try {
     const tasks = await axios({
-      headers: { 'Access-Control-Allow-Origin': 'https://sage-dragon-5c5b26.netlify.app/' },
+      headers: { 'Access-Control-Allow-Origin': 'https://sage-dragon-5c5b26.netlify.app' },
       method,
       url,
       data: _payload,
