4333156594eb1290f65ef8c662c0d89e82a8920d
lion <lionffl@gmail.com>
2023-01-20 17:03:01 -0300

    feat: nav added

---
+++ src/components/Nav.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { fetchUser } from '../helpers/fetch';
+import endpoint from '../helpers/endpoints';
+import { AuthContext } from '../context/AuthContext';
+
+export default function Nav({ user }) {
+  const { setUser } = useContext(AuthContext);
+  const handleClick = async () => {
+    await fetchUser('delete', endpoint.auth.logout);
+    setUser('');
+  };
+  return (
+    <div className="nav-container">
+      <span>
+        Hello,
+        {' '}
+        {user}
+        !
+      </span>
+      <button
+        className="btn btn-edit btn-task"
+        onClick={handleClick}
+        type="button"
+      >
+        Logout
+
+      </button>
+    </div>
+  );
+}
+
+Nav.propTypes = {
+  user: PropTypes.string.isRequired,
+};
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import TaskMenu from '../components/TaskMenu';
 import Flash from '../components/Flash';
 import TasksList from '../components/TasksList';
+import { AuthContext } from '../context/AuthContext';
+import Nav from '../components/Nav';
 
 export default function Tasks() {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!user) navigate('/');
+  }, [user]);
+
   return (
     <div className="App">
+      <Nav user={user} />
       <Flash />
       <TaskMenu />
       <TasksList />
