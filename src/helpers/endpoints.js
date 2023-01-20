f0b1e584c8c53a73b1e425a1f6aea9c261525794
lion <lionffl@gmail.com>
2023-01-20 16:18:39 -0300

    login added

--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -2,6 +2,7 @@ const endpoint = {
   tasks: process.env.REACT_APP_BACKEND_TASK,
   register: process.env.REACT_APP_REGISTER,
   auth: {
+    loggedIn: process.env.REACT_APP_BACKEND_LOGGED,
     login: process.env.REACT_APP_BACKEND_LOGIN,
     logout: process.env.REACT_APP_BACKEND_LOGOUT,
   },
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,85 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
+import { FlashContext } from '../context/FlashContext';
+import endpoint from '../helpers/endpoints';
+import { fetchUser } from '../helpers/fetch';
+import Flash from '../components/Flash';
 
 export default function Login() {
-  const navToTasks = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { setUser } = useContext(AuthContext);
+  const { flash } = useContext(FlashContext);
+  const navigate = useNavigate();
+
+  const handleChange = ({ target }) => {
+    const { value, name: option } = target;
+    const inputs = new Map();
+    inputs.set('username', () => setUsername(value));
+    inputs.set('password', () => setPassword(value));
+    inputs.get(option)();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const payload = {
+      user: {
+        username, password,
+      },
+    };
+
+    const data = await fetchUser('post', endpoint.auth.login, payload);
+    if (data.login) {
+      setUser(data.username);
+      navigate('/tasks');
+    } else {
+      flash(data.message);
+    }
+  };
+
   return (
-    <div className="auth-container">
-      <h1>Sign in</h1>
-      <br />
-      <br />
-      <fieldset>
-        <legend>Credentials</legend>
-        <input
-          placeholder="Login"
-          type="text"
-        />
-        <input
-          placeholder="Password"
-          type="password"
-        />
+    <>
+      <Flash />
+      <form onSubmit={handleSubmit} className="auth-container">
+        <h1>Sign in</h1>
         <br />
-        <p className="login-new-user">
-          New user?
+        <br />
+        <fieldset>
+          <legend>Credentials</legend>
+          <input
+            placeholder="Login"
+            value={username}
+            onChange={handleChange}
+            name="username"
+            type="text"
+          />
+          <input
+            placeholder="Password"
+            value={password}
+            onChange={handleChange}
+            name="password"
+            type="password"
+          />
+          <br />
+          <p className="login-new-user">
+            New user?
+            {' '}
+            <Link to="/signup">Sign up</Link>
+          </p>
+        </fieldset>
+        <button
+          className="btn btn-login"
+          type="submit"
+        >
+          Sign in
+        </button>
+        <p className="forget">
+          Forgot your password?
           {' '}
-          <Link to="/signup">Sign up</Link>
+          <Link to="/recover">Recover</Link>
         </p>
-      </fieldset>
-      <button
-        className="btn btn-login"
-        type="button"
-        onClick={() => navToTasks('/tasks')}
-      >
-        Sign in
-      </button>
-      <p className="forget">
-        Forgot your password?
-        {' '}
-        <Link to="/recover">Recover</Link>
-      </p>
-    </div>
+      </form>
+    </>
   );
 }
